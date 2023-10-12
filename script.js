64d1c3f9873ada2d2822ec8c6ac8928d43648155
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-12 10:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+button.addEventListener("click",()=>{
+	para.innerText = ""
+	para.innerHTML = `<h1>Enter the Metaverse</h1>`;
+})
